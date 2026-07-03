101ec3bc467ed0fc3c342e16135444a5eda60657
gan19920108-bit <gan19920108@gmail.com>
2026-07-03 21:34:21 +0800

    Delete gufen-rpg directory

--- gufen-rpg/src/sillytavern/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export * from './database';
-export * from './lorebook-engine';
-export * from './prompt-assembler';
-export * from './stream-parser';
-export * from './vars-merger';
-export * from './importer';
